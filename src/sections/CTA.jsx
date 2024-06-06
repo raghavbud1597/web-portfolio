4a500005d8c45b7d85fcc298ad77ac9d315f9503
raghavbud1597 <raghav1597@gmail.com>
2024-06-05 21:28:40 -0400

    Updated CTA section Text

--- src/sections/CTA.jsx
+++ src/sections/CTA.jsx
@@ -5,7 +5,7 @@ import Button  from "./components/Button";
 const CTA = () => {
   return (
     <section
-      className={`w-full border-2 rounded-[3rem] border-slate-100 bg-slate-700 ${layout.sectionReverse} md:px-10 lg:px-40 justify-center`}
+      className={`w-full border-2 rounded-[3rem] border-slate-100 bg-slate-700 ${layout.section} md:px-10 lg:px-30 justify-center`}
     >
       <div className={`${styles.flexCenter} min-h-[200px] px-20`}>
         <div className="relative">
@@ -42,7 +42,7 @@ const CTA = () => {
       </div>
       
       <div className={`flex justify-center items-center md:items-start  px-10 md:px20 py-10 md:py-0 flex-col`}>
-        <h2 className={`font-poppins font-semibold md:text-[48px] text-[40px] md:leading-[52px] leading-[44px] text-white text-left`}>Let's work together</h2>
+        <h2 className={`font-poppins font-semibold text-4xl text-white text-left`}>Discover how I breathe life into design through code, and reach out to discuss your project today!</h2>
         <div className="flex gap-2 flex-col lg:flex-row my-4">
           <Button text="Contact Me" link="mailto:raghavbudhiraja78@gmail.com" />
           <Button text="Download CV" link="https://drive.google.com/file/d/1mAyc7CaOj8kSDrpDFpXbsq-lBHPX89gj/view?usp=drive_link" />
